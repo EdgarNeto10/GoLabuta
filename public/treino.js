da6137888780c56e43faaea539c303cf6438bc4b
Edgar Neto <edgar100neto@gmail.com>
2019-11-05 21:35:35 +0000

    Prototipo project plan

--- public/treino.js
+++ public/treino.js
@@ -1,9 +1,18 @@
 
-//window.alert("GoLabuta") ;
-
+//window.alert("GoLabuta")
 
 
 function clicar()
 {
     document.getElementById("enviar").value="Enviado"
-}
+    var cont=0;
+    if(cont==0){
+        $('#formu').submit();
+    }
+} 
+
+  /*$.post('',function(data,status)){
+  $(id).html(data);
+  
+
+}*
